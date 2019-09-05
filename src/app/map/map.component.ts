a166dd023a52d7d69128196b0767e163a4e018df
Prischep <ya.pryshchep@yandex.by>
2019-09-05 23:19:29 +0300

    added map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  @ViewChild('map', {static: true})
+  mapElement: any;
+  map: google.maps.Map;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+  }
+
+}
