c5ec1593935504bd5d0a9ab0c8c32753b64e156a
Prischep <ya.pryshchep@yandex.by>
2019-09-08 23:42:16 +0300

    added polygon drawing

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DeleteMenu } from '../model/DeleteMenu';
 
 @Component({
   selector: 'app-map',
@@ -10,17 +11,52 @@ export class MapComponent implements OnInit {
   @ViewChild('map', {static: true})
   mapElement: any;
   map: google.maps.Map;
+  poly = null;
 
   constructor() {
   }
 
   ngOnInit() {
-    const mapProperties = {
-      center: new google.maps.LatLng(35.2271, -80.8431),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 7,
+      center: {lat: 41.879, lng: -87.624}
+    });
+
+    this.poly = new google.maps.Polyline({
+      editable: true,
+      strokeOpacity: 0.7,
+      strokeColor: '#75d8ff',
+      strokeWeight: 2,
+      draggable: true
+    });
+    this.poly.setMap(this.map);
+
+    this.map.addListener('click', this.addLatLng.bind(this));
+
+    const deleteMenu = new DeleteMenu();
+
+    google.maps.event.addListener(this.poly, 'rightclick', (e) => {
+      if (e.vertex === undefined) {
+        return;
+      }
+      deleteMenu.open(this.map, this.poly.getPath(), e.vertex);
+    });
+
+    google.maps.event.addListener(this.poly, 'click', (e) => {
+      const poly = this.poly.getPath();
+
+      if (e.vertex === 0 && this.poly.getPath().getLength() > 2) {
+        poly.push(poly.getAt(0));
+      }
+    });
   }
 
+  addLatLng(event) {
+    const path = this.poly.getPath();
+    if (path.getAt(0) === path.getAt(path.getLength() - 1) && path.getLength() > 1) {
+      return;
+    }
+
+    path.push(event.latLng);
+  }
 }
